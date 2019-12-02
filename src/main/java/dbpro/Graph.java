f9bee652cf9c11c77f141f9c35e336156aa8def0
mathis98 <mathis.arend@gmail.com>
2019-12-02 10:44:00 +0100

    Update graph.java

--- src/main/java/dbpro/Graph.java
+++ src/main/java/dbpro/Graph.java
@@ -1,12 +1,9 @@
 package dbpro;
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
-import org.neo4j.driver.v1.Transaction;
-import org.neo4j.driver.v1.TransactionWork;
+import org.neo4j.driver.v1.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
@@ -20,22 +17,37 @@ public class Graph implements AutoCloseable
     }
 
     @Override
-    public void close() throws Exception
-    {
+    public void close() {
         driver.close();
     }
 
-    public void addStation( final String name ) {
+    public void addStation(final String name ) {
+        String queryText = "CREATE (a:Station) " +
+                "SET a.name = $name " +
+                "RETURN a.name + ', from node ' + id(a)";
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        runQuery(queryText, params);
+    }
+
+    public void addConnection(String from, String to) {
+
+    }
+
+    public void addConnection(int from, int to) {
+
+    }
+
+    private void addConnectionGraph() {
+
+    }
+
+    private void runQuery(String query, Map<String, Object> params) {
         try (Session session = driver.session()) {
-            String trans = session.writeTransaction(new TransactionWork<String>() {
-                @Override
-                public String execute(Transaction tx) {
-                    StatementResult result = tx.run("CREATE (a:Station) " +
-                                    "SET a.name = $name " +
-                                    "RETURN a.name + ', from node ' + id(a)",
-                            parameters("name", name));
-                    return result.single().get(0).asString();
-                }
+            String trans = session.writeTransaction(tx -> {
+                StatementResult result = tx.run(query,
+                        parameters(params));
+                return result.single().get(0).asString();
             });
             System.out.println(trans);
         }
