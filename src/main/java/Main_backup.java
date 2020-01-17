c9ebc1e9567d2ae8dbfb3163894d1f31e70feac1
mathis98 <mathis.arend@gmail.com>
2020-01-17 14:48:48 +0100

    Remove unused stuff from Main.java + create Main_backup.java of old stuff

---
+++ src/main/java/Main_backup.java
@@ -0,0 +1,52 @@
+import dbpro.JSONParser;
+import dbpro.Journey;
+import dbpro.Station;
+import dbpro.dbApiRequest;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main_backup {
+    static String password = "1111";
+    static String uri = "bolt://localhost:7687";
+    static String user = "neo4j";
+
+
+    public static ArrayList<Station> stationList;
+
+
+    public static void main(String... args) throws Exception {
+        // List for storing all the journeys
+        HashMap<String, Journey> journeys = new HashMap<>();
+
+        // new dbAPIRequest
+        dbApiRequest api = new dbApiRequest();
+
+        // create a new instance of JSONParser
+        JSONParser parser = new JSONParser();
+
+        /*
+        GET STATION DATA
+         */
+        for (int i = 0; i <= 100; i++) {
+
+            String line = String.valueOf(i);
+
+            try {
+                File file = new File(String.format("D:\\Dev\\Java\\DBPRO\\src\\main\\java\\dbpro\\json\\%s.json", line));
+
+                FileReader reader = new FileReader(file);
+
+                stationList = parser.parseJson(reader, line);
+
+
+            } catch (Exception e) {
+
+            }
+        }
+        stationList.stream().forEach(a -> a.writeLineInfo());
+         stationList.stream().forEach(a -> System.out.println("Station: " + a.getEvaID() + " size of infoList: " + a.getLineInfoList().size()));
+    }
+}
