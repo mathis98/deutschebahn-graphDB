4ecbd4f199ab6c602c4e64f8b2653bded45e47d9
mathis98 <mathis.arend@gmail.com>
2020-01-17 15:28:42 +0100

    Fix Main vs Main_backup

--- src/main/java/Main_backup.java
+++ src/main/java/Main_backup.java
@@ -50,3 +50,82 @@ public class Main_backup {
          stationList.stream().forEach(a -> System.out.println("Station: " + a.getEvaID() + " size of infoList: " + a.getLineInfoList().size()));
     }
 }
+
+
+//        String ids = dbApiRequest.getIds();
+//        System.out.println(ids);
+//        ArrayList<Station> stationList = parser.parse(ids);
+//        System.out.println("length: " + stationList.size());
+//
+////         print result
+//        System.out.println("STATIONS: ");
+//        stationList.forEach(a -> System.out.println(a.toString()));
+//        System.out.println("\n");
+
+// for each station
+//        for(Station station : stationList) {
+//            System.out.println("Departures from " + station.getName());
+//            /*
+//            GET DEPARTURE INFO
+//             */
+//            // TODO: this api request only returns 20 stops! loop?
+//            int hours = 0;
+//            int minutes = 0;
+//            ArrayList<Stop> stopList = new ArrayList<>();
+//            int oldHours = -1;
+//            int oldMinutes = -1;
+//            while(hours < 24) {
+//                String departure = api.getDeparture(
+//                        station.getEvaID(),
+//                        "2019-12-27T" + String.format("%02d", hours) + ":" + String.format("%02d", minutes));
+//                stopList.addAll(
+//                        parser.parseStops(
+//                                departure,
+//                                new Station("Berlin Hbf", 8011160, null, null)
+//                        )
+//                );
+//                if(stopList.size() == 0) break;
+//                Collections.sort(stopList);
+//                String timeString = stopList.get(stopList.size() - 1).getDepartureTime();
+//                timeString = timeString.substring(11);
+//                hours = Integer.parseInt(timeString.substring(0,2));
+//                minutes = Integer.parseInt(timeString.substring(3));
+//                minutes += 1;
+//                if(minutes >= 60) {
+//                    minutes = 0;
+//                    hours += 1;
+//                }
+//                if(oldHours == hours && oldMinutes == minutes) break;
+//                oldHours = hours;
+//                oldMinutes = minutes;
+//            }
+//
+//            System.out.println("got " + stopList.size() + " stops");
+//            if(stopList.size() == 0) {
+//                System.out.println("so we can skip this station");
+//                continue;
+//            }
+//            System.out.println("");
+//
+//            // for each stop
+//            for(Stop stop: stopList) {
+//                // if journey doesn't exist in journeys array
+//                if (!journeys.containsKey(stop.getDetailsId())) {
+//                    // get journey from /journeyDetails and add as new journey
+//                }
+//            }
+//
+//            // add track if necessary
+//        }
+//
+//        /*
+//        ADD EVERYTHING TO THE GRAPH/ UPDATE GRAPH
+//         */
+//       try ( Graph graph = new Graph( uri, user, password ) )
+//       {
+//           // add stations to graph
+////           stationList.forEach(s -> graph.addStation(s.getName(),s.getEvaID(),s.getLongtitude(),s.getLatitude()));
+//
+//        } catch(Exception e) {
+//            System.out.println(e.getMessage());
+//        }
